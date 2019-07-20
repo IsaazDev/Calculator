bb9c58799d8667664b33b969113b3e0f25e41790
IsaazDev <52111238+IsaazDev@users.noreply.github.com>
2019-07-20 16:52:02 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(97, 35);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 0;
+            this.button11.Text = "Copy";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(97, 84);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 1;
+            this.button12.Text = "Delete";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(97, 140);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 23);
+            this.button13.TabIndex = 2;
+            this.button13.Text = "Move file";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(97, 204);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 23);
+            this.button14.TabIndex = 3;
+            this.button14.Text = "Move Back";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(354, 51);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(294, 20);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.DragDrop += new System.Windows.Forms.DragEventHandler(this.Dropfile);
+            this.textBox5.DragEnter += new System.Windows.Forms.DragEventHandler(this.Openfile);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(354, 115);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(294, 20);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.DragDrop += new System.Windows.Forms.DragEventHandler(this.DropFiles2);
+            this.textBox6.DragEnter += new System.Windows.Forms.DragEventHandler(this.OpenFiles2);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(354, 181);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(75, 23);
+            this.button15.TabIndex = 6;
+            this.button15.Text = "Copy Dir";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(571, 181);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(75, 23);
+            this.button16.TabIndex = 7;
+            this.button16.Text = "Move Dir";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(463, 181);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(75, 23);
+            this.button17.TabIndex = 8;
+            this.button17.Text = "Delete Dir";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.Button17_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.FolderBrowserDialog1_HelpRequest);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(693, 261);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+// ctlr 
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Copy
+        private void button11_Click(object sender, EventArgs e)
+        {
+            var fileToCopy = "C:\\Users\\deleo\\Desktop\\Dele Isaac Oyinsan..docx";
+            var newLocation = "C:\\Users\\deleo\\Desktop\\new\\Dele Isaac Oyinsan..docx";
+            var findDirectory = "C:\\Users\\deleo\\Desktop";
+
+            if (Directory.Exists(findDirectory))
+            {
+                if (File.Exists(fileToCopy))
+                {
+                    File.Copy(fileToCopy, newLocation, true);
+                    MessageBox.Show("File has been copied!");
+                }
+                else
+                {
+                    MessageBox.Show("Please find the correct file");
+                }
+            }
+            else
+            {
+
+                MessageBox.Show("Not available");
+            }
+        }
+        //Delete
+        private void button12_Click(object sender, EventArgs e)
+        {
+            var DoNotExist = "C:\\Users\\deleo\\Desktop\\new\\Dele Isaac Oyinsan..docx";
+
+            File.Delete(DoNotExist);
+            MessageBox.Show("File Gone");
+        }
+        //Move 
+        private void button13_Click(object sender, EventArgs e)
+        {
+            var FileLoc = "C:\\Users\\deleo\\Desktop\\Dele Isaac Oyinsan..docx";
+            var WhereFile = "C:\\Users\\deleo\\Desktop\\new\\Dele Isaac Oyinsan..docx";
+            if (File.Exists(FileLoc))
+            {
+                File.Move(FileLoc, WhereFile);
+                //Directory.Move("C:\\Users\\deleo\\Desktop\\Dele Isaac Oyinsan.", "C:\\Users\\deleo\\Desktop\\new");
+                MessageBox.Show("Yaay! it work");
+            }
+            else
+                MessageBox.Show("Sorry, this file doesn't Exsit :(!");
+        }
+        // Move there and back
+        private void button14_Click(object sender, EventArgs e)
+        {
+            var MoveBack = "C:\\Users\\deleo\\Desktop\\new\\Dele Isaac Oyinsan..docx";
+            var backMove = "C:\\Users\\deleo\\Desktop\\Dele Isaac Oyinsan..docx";
+
+            if (File.Exists(MoveBack))
+            {
+                File.Move(MoveBack, backMove);
+                MessageBox.Show("Back to where you came from!");
+            }
+
+            else
+            {
+                MessageBox.Show("I wasn't here :(");
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Move mulitple file 
+        private void Dropfile(object sender, DragEventArgs e)
+        {
+            var Newfile = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            foreach (var Newfiles in Newfile)
+                textBox5.Text = Newfiles;
+        }
+
+        private void Openfile(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+                e.Effect = DragDropEffects.All;
+        }
+
+        private void DropFiles2(object sender, DragEventArgs e)
+        {
+            var Newfile2 = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            foreach (var Newfiles2 in Newfile2)
+                textBox6.Text = Newfiles2;
+
+
+        }
+
+        private void OpenFiles2(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+                e.Effect = DragDropEffects.All;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            var CreateFile = Directory.Exists("C:\\Users\\deleo\\Desktop\\NewFolder");
+            // var Createfile = Directory.SetAccessControl("C:\\Users\\deleo\\Desktop\\NewFolder";)
+
+            //  //this is a loop i created, hoping I can make it create more than one file 
+            //for (int i = Createfile, i < 1, i++)
+            // {
+            //     messagebox.show("it making more!")
+            //  }
+            try
+            {
+            if (CreateFile)
+                MessageBox.Show("It already exsit");
+            else if (!CreateFile)
+            {
+                Directory.CreateDirectory("C:\\Users\\deleo\\Desktop\\NewFolder");
+                MessageBox.Show("this has been Created! :)");
+            }
+          
+                string filelocation = textBox5.Text;
+                string[] Filebutton = Directory.GetFiles(filelocation);
+                string Movinglocation = textBox6.Text;
+
+                foreach (var file in Filebutton)
+                {
+                    var NameFile = Path.GetFileName(file);
+                    File.Copy(file, Movinglocation + "\\" + NameFile, true);
+                    // File.Delete(file);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            var CreateFile = Directory.Exists("C:\\Users\\deleo\\Desktop\\NewFolder");
+            //  //this is a loop i created, hoping I can make it create more than one file 
+            //  for (int i = CreateFile, i < 17, i++)
+            //    {
+            //      MessageBox.Show("it making more!")
+            //    }
+
+            if (CreateFile)
+                MessageBox.Show("It already exsit");
+            else if (!CreateFile)
+            { 
+                Directory.CreateDirectory("C:\\Users\\deleo\\Desktop\\NewFolder");
+
+                MessageBox.Show("this has been Created! bdecause Mary Anne bullied me! :D, Hope everyone had a Dounut!");
+            }
+            
+                try
+                {
+                     MessageBox.Show("It already There, I told you!");
+                    var filelocation = textBox5.Text;
+                    var Filebutton = Directory.GetFiles(filelocation);
+                    var CheckCreatedtime = Directory.GetCreationTime("C:\\Users\\deleo\\Desktop\\Docx");
+                    var Movinglocation = textBox6.Text;
+
+                    foreach (var file in Filebutton)
+                    {
+                        var NameFile = Path.GetFileName(file);
+                        File.Copy(file, Movinglocation + "\\" + NameFile, true);
+                        File.Delete(file);
+                        //File.Move()
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+        }
+
+        private void Button17_Click(object sender, EventArgs e)
+        {
+            File.Delete("C:\\Users\\deleo\\Desktop\\NewFolder");
+            MessageBox.Show("It has Gone");
+        }
+
+        private void FolderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+    //private void button10_Click(object sender, EventArgs e)
+    //{
+
+    //    //try
+    //    //{
+
+    //    //    var filelocation = textBox1.Text;
+    //    //    var Filebutton = Directory.GetFiles(filelocation);
+    //    //    var Movinglocation = textBox2.Text;
+
+    //    //    foreach (var file in Filebutton)
+    //    //    {
+    //    //        var NameFile = Path.GetFileName(file);
+    //    //        File.Copy(file, Movinglocation + "\\" + NameFile, true);
+    //    //        // File.Delete(file);
+    //    //    }
+    //    //}
+    //    //catch (Exception ex)
+    //    //{
+    //    //    MessageBox.Show(ex.ToString());
+    //    //}
+    //}
+
+
+}
+
+//}
+
+
+
+
+
+
+
+
+
+//private void textBox1_TextChanged(object sender, DragEventArgs e)
+//{
+//    var CopyFile = (string[])e.Data.GetData(DataFormats.FileDrop, false); 
+//    foreach(var newFile in CopyFile)
+//    {
+//        textBox1.Text = newFile;
+//    }
+//}
+
+//    }
+//}
+
